ab2e6f68284e81d091f8fdd59ea275da6e940b0b
Marco Mechini <mechinimarco35@gmail.com>
2024-11-15 14:13:48 +0100

    feat: snack 2 complete

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,7 +3,4 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-names.forEach(curName => {
-    console.log(curName);
-
-})
+names.forEach(curName => console.log(curName))

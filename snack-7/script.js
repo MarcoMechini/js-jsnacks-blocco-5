69c33165904be7b89d7c8229b2296679af88de83
Marco Mechini <mechinimarco35@gmail.com>
2024-11-15 14:47:27 +0100

    feat: snack 7 complete

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let student2 = null;
+students.forEach(curStud => {
+  if (curStud.id === 2) student2 = { ...curStud }
+})
+
+console.log(student2);
